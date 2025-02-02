bf5faf2a57821cb1a76826990ca7a63f138b4e85
Egor <71181173+Rollfreeze@users.noreply.github.com>
2025-02-02 23:53:25 +0300

    Add grpcCleint call

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,40 @@
+// ignore_for_file: use_build_context_synchronously
 import 'package:flutter/material.dart';
+import 'package:flutter_grpc_client/network/client.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  final grpcCleint = GrpcClient.create();
+  String answer = '';
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () => grpcCleint.greetServer(cleintName: 'Cleint').then(
+                      (value) => setState(() => answer = value),
+                    ),
+                child: Text('Say Hello to Server'),
+              ),
+              const SizedBox(height: 10),
+              Text('Current answer is: $answer'),
+            ],
+          ),
         ),
       ),
     );
